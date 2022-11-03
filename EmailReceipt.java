16cf3fe6a3a8926006d7f7f14420140aaf2b4dc5
ABDULRHMAN MOHAMMED A ALMEHDAR <83496936+AMM48@users.noreply.github.com>
2022-11-03 13:39:13 +0300

    Add files via upload

---
+++ EmailReceipt.java
@@ -0,0 +1,32 @@
+package edu.kau.fcit.cpit252.receiptStrategy;
+
+import edu.kau.fcit.cpit252.paymentsStrategy.Payment;
+import edu.kau.fcit.cpit252.shopping.Product;
+import edu.kau.fcit.cpit252.utils.EmailTemplate;
+import edu.kau.fcit.cpit252.utils.SendEmail;
+
+import java.util.List;
+
+public class EmailReceipt extends Receipt {
+    
+    private String subject;
+    private String recipient;
+
+    public EmailReceipt(String subject, String recipient, List<Product> products) {
+        super(products);
+        this.subject = subject;
+        this.recipient = recipient;
+    }
+
+    @Override
+    public void generate() {
+        try {
+            System.out.println("Receipt has been sent to " + recipient);
+        }
+        catch(Exception e){
+            System.err.println("Failed to email the receipt: Reason: " + e.getMessage());
+            return;
+        }
+
+    }
+}
